740200f54bba477cf26e48919c94bf376efa85ed
bingjiegu <bingjie07191223@sina.com>
2019-08-20 20:39:45 +0800

    1.新增重复flow统计方法

--- singl_api/util/get_host.py
+++ singl_api/util/get_host.py
@@ -1,8 +1,10 @@
 from urllib.parse import urlparse
 
 def get_host(url):
+    print(url)
     host = urlparse(url).scheme + '://' + urlparse(url).netloc
+    print(host)
     return host
 
-url = 'http://192.168.1.57:8515/api/datasets/name/'
-print(get_host(url), type(get_host(url)))
+# url = 'http://192.168.1.57:8515/api/datasets/name/'
+# # # print(get_host(url), type(get_host(url)))
