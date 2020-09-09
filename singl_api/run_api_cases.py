22e69c464ff9931cccc2d9d8af1d78ce203f79ff
jinbo.Guo <guojinbo2006@126.com>
2020-09-09 13:48:50 +0800

    更新

--- singl_api/run_api_cases.py
+++ singl_api/run_api_cases.py
@@ -4,7 +4,7 @@ import datetime
 from new_api_cases.execute_cases import deal_request_method
 from new_api_cases.execute_cases import CheckResult
 from basic_info.setting import host
-from basic_info.setting import receivers_list,receivers_test
+from basic_info.setting import receivers_list
 
 # from newSuite import NewSuite
 
