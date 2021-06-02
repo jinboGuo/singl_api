be22b6ef2b41b9aaba6e343d74d525ba2a3dc2e7
jinbo.Guo <guojinbo2006@126.com>
2021-06-02 17:58:42 +0800

    dw 添加145 tenant

--- singl_api/new_api_cases/execute_dw_cases.py
+++ singl_api/new_api_cases/execute_dw_cases.py
@@ -25,7 +25,7 @@ from new_api_cases.dw_prepare_datas import update_business_data, \
 ms = MYSQL(Dw_MySQL_CONFIG["HOST"], Dw_MySQL_CONFIG["USER"], Dw_MySQL_CONFIG["PASSWORD"], Dw_MySQL_CONFIG["DB"], Dw_MySQL_CONFIG["PORT"])
 ab_dir = lambda n: os.path.abspath(os.path.join(os.path.dirname(__file__), n))
 case_table = load_workbook(ab_dir("api_cases.xlsx"))
-case_table_sheet = case_table.get_sheet_by_name('dw')
+case_table_sheet = case_table.get_sheet_by_name('842')
 all_rows = case_table_sheet.max_row
 jar_dir = ab_dir('woven-common-3.0.jar')
 log = Logger().get_log()
--- singl_api/util/get_tenant.py
+++ singl_api/util/get_tenant.py
@@ -18,14 +18,14 @@ def get_env_num(host):
 
 def get_tenant(host):
     host_env_num = get_env_num(host)
-    tenant_id_189 = "2d7ad891-41c5-4fba-9ff2-03aef3c729e5"  # 189环境default租戶ID
+    tenant_id_145 = "a5a4b81e-d2a6-498d-9ff0-3a627d3d5b5a"  # 145环境default租戶ID
     tenant_id_81 = "55f7f910-b1c9-41d2-9771-e734e6b8285f"  # 81环境default租戶ID
     tenant_id_82 = "db09f359-1e4d-4b3c-872e-7775bd8eed8b"
     tenant_id_83 = "e5188f23-d472-4b2d-9cfa-97a0d65994cf"
     tenant_id_84 = "8c488afc-e9d7-42af-b127-f8a1412ba50e"
     tenant_id_199 = "39823d2e-7998-4d0e-a3e7-5edeecba0dc2"
-    if host_env_num == '189':
-        return tenant_id_189
+    if host_env_num == '145':
+        return tenant_id_145
     elif host_env_num == '81':
         return tenant_id_81
     elif host_env_num == "84":
@@ -40,7 +40,7 @@ def get_tenant(host):
         print("目前只处理189,81,82,83,84环境tenant，若不包含在内，请添加")
 
 def get_owner():
-    ms = MYSQL(Dw_MySQL_CONFIG["HOST"], Dw_MySQL_CONFIG["USER"], Dw_MySQL_CONFIG["PASSWORD"], Dw_MySQL_CONFIG["DB"])
+    ms = MYSQL(Dw_MySQL_CONFIG["HOST"], Dw_MySQL_CONFIG["USER"], Dw_MySQL_CONFIG["PASSWORD"], Dw_MySQL_CONFIG["DB"], Dw_MySQL_CONFIG["PORT"])
     try:
       osql = "select id from merce_user where creator='SYSTEM' and last_modifier='admin' and name='admin' "
       owners = ms.ExecuQuery(osql)
